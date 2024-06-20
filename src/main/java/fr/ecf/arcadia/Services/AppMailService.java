bb349cc5d6560ed51fefd62df82715920da4916e
Denis Tavernier <denis.tavernier@gamil.com>
2024-06-20 15:00:17 +0200

    Fix: mailtrap connection failed

--- src/main/java/fr/ecf/arcadia/Services/AppMailService.java
+++ src/main/java/fr/ecf/arcadia/Services/AppMailService.java
@@ -30,16 +30,28 @@ public class AppMailService {
 
         System.out.println("================================> connect mail." + environment.getProperty("mail.smtp.username") + "     " + environment.getProperty("mail.smtp.host"));
 
+        prop.put("mail.smtp.username", environment.getProperty("mail.smtp.username"));
+        prop.put("mail.smtp.password", environment.getProperty("mail.smtp.password"));
         prop.put("mail.smtp.auth", environment.getProperty("mail.smtp.auth"));
         prop.put("mail.smtp.starttls.enable", environment.getProperty("mail.smtp.starttls.enable"));
         prop.put("mail.smtp.host", environment.getProperty("mail.smtp.host"));
         prop.put("mail.smtp.port", environment.getProperty("mail.smtp.port"));
         prop.put("mail.smtp.ssl.trust", environment.getProperty("mail.smtp.ssl.trust"));
+        // prop.put("mail.smtp.auth.mechanisms", "LOGIN");
+        // prop.put("mail.smtp.auth.mechanisms", "smtp");
     
+        // prop.put("spring.mail.password", "Weddingcard.1");
+        // prop.put("spring.mail.password", "Weddingcard.1");
+        // // prop.put("spring.mail.port", "587");
+        // prop.put("spring.mail.properties.mail.smtp.starttls.enable", true);
+        // prop.put("spring.mail.properties.mail.smtp.auth", true);
+        // prop.put("spring.mail.properties.mail.smtp.starttls.required", true);
+
+        // spring.mail.properties.mail.smtp.starttls.required=true
         this.session = Session.getInstance(prop, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                System.out.println("================================> connect mail." + environment.getProperty("mail.smtp.username") + "     " + environment.getProperty("mail.smtp.host"));
+                System.out.println("================================> connect mail." + environment.getProperty("mail.smtp.username") + "     " + environment.getProperty("mail.smtp.password"));
                 return new PasswordAuthentication(environment.getProperty("mail.smtp.username"), environment.getProperty("mail.smtp.password"));
             }
         }); 
--- src/main/java/fr/ecf/arcadia/Services/ContactServiceImpl.java
+++ src/main/java/fr/ecf/arcadia/Services/ContactServiceImpl.java
@@ -4,53 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.ecf.arcadia.pojo.Contact;
-import jakarta.mail.Message;
-import jakarta.mail.MessagingException;
-import jakarta.mail.Multipart;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeBodyPart;
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.internet.MimeMultipart;
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 @Service
 public class ContactServiceImpl implements ContactService {
 
     @Autowired
-    private AppMailService mailService;
+    private JavaMailSender mailSender;
 
-    @Override
     public String sendMail(Contact contact) {
 
-        MimeMessage message = new MimeMessage(mailService.getSession());
-        System.out.println("================================> Envoi du mail.");
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("arcadia@test.com");
+        message.setSubject(contact.getTitle());
+        message.setText(contact.getDescription());
+
+        mailSender.send(message);
         
-        try {
-
-            message.setFrom(new InternetAddress("from@etupdt.fr"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(contact.getEmail()));
-            message.setSubject(contact.getTitle());
-
-            String msg = contact.getDescription();
-
-            MimeBodyPart mimeBodyPart = new MimeBodyPart();
-            mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
-
-            Multipart multipart = new MimeMultipart();
-            multipart.addBodyPart(mimeBodyPart);
-
-            message.setContent(multipart);
-
-            Transport.send(message);
-            return "Le mail a été envoyé.";
-
-        } catch (MessagingException e) {
-
-            System.out.println("================================> Envoi du mail." + e.getMessage());
-            return "Erreur lors de l'envoi du mail.";
-
-        }
+        return "ok";
 
     }
-    
 }
