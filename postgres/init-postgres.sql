da1317425bacc8e2fd4d013141cb4a1105ed1e14
Denis Tavernier <denis.tavernier@gamil.com>
2024-07-16 14:46:06 +0200

    Config: add demo properties

--- postgres/init-postgres.sql
+++ postgres/init-postgres.sql
@@ -149,4 +149,4 @@ ALTER SEQUENCE token_seq OWNED BY token.id;
 
 INSERT INTO users
 ("role", email, firstname, lastname, "password")
-VALUES(2, 'admin@test.com', 'José', 'Duseau', '$2a$10$hTc/.Qpwe0I/hSwwmBXX1eQvHI8aqvGlxJ7VKXE5SKkkrYLibsYwa');
+VALUES(2, 'admin@test.com', 'José', 'Duseaux', '$2a$10$hTc/.Qpwe0I/hSwwmBXX1eQvHI8aqvGlxJ7VKXE5SKkkrYLibsYwa');
